d47eb76ad722e680ca7f9c0115f3c0bb6fab10b9
Basile Trujilllo [L0gIn] <basiletrujillo@gmail.com>
2017-07-28 14:16:09 +0200

    Remove EB App and Add EFS mount settings

---
+++ eb-env/main.tf
@@ -0,0 +1,258 @@
+##################################################
+## AWS config
+##################################################
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+##################################################
+## IAM Roles and profiles
+##################################################
+resource "aws_iam_instance_profile" "beanstalk_service" {
+  name = "${var.service_name}-${var.env}-beanstalk-service-user"
+  role = "${aws_iam_role.beanstalk_service.name}"
+}
+resource "aws_iam_instance_profile" "beanstalk_ec2" {
+  name = "${var.service_name}-${var.env}-beanstalk-ec2-user"
+  role = "${aws_iam_role.beanstalk_ec2.name}"
+}
+resource "aws_iam_role" "beanstalk_service" {
+  name = "${var.service_name}-${var.env}-beanstalk-service-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "elasticbeanstalk.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "elasticbeanstalk"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role" "beanstalk_ec2" {
+  name = "${var.service_name}-${var.env}-beanstalk-ec2-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_policy_attachment" "beanstalk_service" {
+  name = "${var.service_name}-${var.env}-elastic-beanstalk-service"
+  roles = ["${aws_iam_role.beanstalk_service.id}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
+}
+resource "aws_iam_policy_attachment" "beanstalk_service_health" {
+  name = "${var.service_name}-${var.env}-elastic-beanstalk-service-health"
+  roles = ["${aws_iam_role.beanstalk_service.id}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
+}
+resource "aws_iam_policy_attachment" "beanstalk_ec2_web" {
+  name = "${var.service_name}-${var.env}-elastic-beanstalk-ec2-web"
+  roles = ["${aws_iam_role.beanstalk_ec2.id}"]
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+
+
+##################################################
+## Elastic Beanstalk
+##################################################
+resource "aws_elastic_beanstalk_environment" "eb_env" {
+  name                = "${var.service_name}-${var.env}"
+  application         = "${var.service_name}"
+  # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts.platforms.html#concepts.platforms.nodejs
+  solution_stack_name = "${var.eb_solution_stack_name}"
+
+  # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
+  # Configure your environment's EC2 instances.
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "${var.instance_type}"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "EC2KeyName"
+    value     = "${var.ssh_key_name}"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "SecurityGroups"
+    value     = "${var.security_groups}"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "${aws_iam_instance_profile.beanstalk_ec2.name}"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = "${aws_iam_instance_profile.beanstalk_service.name}"
+  }
+
+  # Configure your environment to launch resources in a custom VPC
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "${var.vpc_id}"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "${var.vpc_subnets}"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBSubnets"
+    value     = "${var.elb_subnets}"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     = "${var.public_ip}"
+  }
+
+  # Configure your environment's Auto Scaling group.
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = "${var.min_instance}"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = "${var.max_instance}"
+  }
+
+  # Configure rolling deployments for your application code.
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name      = "DeploymentPolicy"
+    value     = "${var.deployment_policy}"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name      = "IgnoreHealthCheck"
+    value     = "${var.ignore_healthcheck}"
+  }
+
+  # Configure your environment's architecture and service role.
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "LoadBalanced"
+  }
+
+  # Configure the default listener (port 80) on a classic load balancer.
+  setting {
+    namespace = "aws:elb:listener:80"
+    name      = "InstancePort"
+    value     = "${var.port}"
+  }
+  setting {
+    namespace = "aws:elb:listener:80"
+    name      = "ListenerEnabled"
+    value     = "${var.enable_http}"
+  }
+
+  # Configure additional listeners on a classic load balancer.
+  setting {
+    namespace = "aws:elb:listener:443"
+    name      = "ListenerProtocol"
+    value     = "HTTPS"
+  }
+  setting {
+    namespace = "aws:elb:listener:443"
+    name      = "InstancePort"
+    value     = "${var.port}"
+  }
+  setting {
+    namespace = "aws:elb:listener:443"
+    name      = "SSLCertificateId"
+    value     = "${var.ssl_certificate_id}"
+  }
+  setting {
+    namespace = "aws:elb:listener:443"
+    name      = "ListenerEnabled"
+    value     = "${var.enable_https}"
+  }
+
+  # Modify the default stickiness and global load balancer policies for a classic load balancer.
+  setting {
+    namespace = "aws:elb:policies"
+    name      = "ConnectionSettingIdleTimeout"
+    value     = "${var.elb_connection_timeout}"
+  }
+
+  # Configure a health check path for your application. (ELB Healthcheck)
+  setting {
+    namespace = "aws:elasticbeanstalk:application"
+    name      = "Application Healthcheck URL"
+    value     = "${var.healthcheck_url}"
+  }
+
+  # Node.js Platform Options
+  # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-specific.html#command-options-nodejs
+  setting {
+    namespace = "aws:elasticbeanstalk:container:nodejs"
+    name      = "NodeCommand"
+    value     = "${var.node_cmd}"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:container:nodejs"
+    name      = "NodeVersion"
+    value     = "${var.node_version}"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:container:nodejs"
+    name      = "ProxyServer"
+    value     = "${var.proxy_server}"
+  }
+
+  # Run the AWS X-Ray daemon to relay trace information from your X-Ray integrated Node.js application.
+  setting {
+    namespace = "aws:elasticbeanstalk:xray"
+    name      = "XRayEnabled"
+    value     = "${var.xray_enable}"
+  }
+
+  # Configure environment properties for your application.
+  # EFS Environment variables
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "EFS_ID"
+    value     = "${var.efs_id}"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "EFS_MOUNT_DIRECTORY"
+    value     = "${var.efs_mount_directory}"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "AWS_REGION"
+    value     = "${var.aws_region}"
+  }
+
+}
