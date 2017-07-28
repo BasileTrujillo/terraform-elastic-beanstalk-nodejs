e19b84bbfac2d4d76636ac7908d62e3fdb217ea5
Basile Trujilllo [L0gIn] <basiletrujillo@gmail.com>
2017-07-28 14:24:25 +0200

    Remove old app module

--- app/outputs.tf
+++
@@ -1,6 +0,0 @@
-##################################################
-## App Variables Output
-##################################################
-output "eb_cname" {
-  value = "${aws_elastic_beanstalk_environment.eb_env.cname}"
-}
